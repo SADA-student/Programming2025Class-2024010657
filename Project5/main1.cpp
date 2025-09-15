b281fa671025a0b8883725aa39f0d320db5bffd5
jeonghun Hwang <cju.sada.24@gmail.com>
2025-09-15 16:53:05 +0900

    main2

--- Project5/main1.cpp
+++ Project5/main1.cpp
@@ -3,11 +3,11 @@
 int main(void)
 {
 	int age = 28;
-	printf("=== Basic Constants ===\n");
-	printf("Name\t:   Jeonghun\n");
-	printf("Initial\t:   J\n");
-	printf("Age\t:   21\n");
-	printf("Score\t:   %.6lf\n",  93.5);
+	printf("=== Basic Constants ===\n\n");
+	printf("Name\t:   Jeonghun\n\n");
+	printf("Initial\t:   J\n\n");
+	printf("Age :   21\n\n");
+	printf("Score\t:   %.6lf\n\n",  93.5);
 	printf("Score\t:   3000000000\n");
 
 	return 0;
