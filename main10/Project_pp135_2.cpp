b286e29483ce162b5e118bc46e8077f223e3a6db
jeonghun Hwang <cju.sada.24@gmail.com>
2025-10-18 09:54:01 +0900

    main10

---
+++ main10/Project_pp135_2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int chest = 95;
+	char size;
+
+	if (chest <= 90)
+	{
+		size = 'S';
+	}
+	else if (chest <= 100)
+	{
+		size = 'M';
+	}
+	else
+	{
+		size = 'L';
+	}
+}
